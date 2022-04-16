5c612035479fd1ee10a3b57cd14602fdccc1f8b7
Wanderson Ferreira <wand@hey.com>
2022-04-16 09:15:01 -0300

    globally ignored directories

--- preferences/+projectile.el
+++ preferences/+projectile.el
@@ -5,5 +5,12 @@
   ;; disable projectile caching
   (setq projectile-enable-caching nil)
 
+  ;; add clojure specific folders to be ignored by projectile
+  (setq projectile-globally-ignored-directories
+        (append projectile-globally-ignored-directories
+                '(".clj-kondo"
+                  ".cpcache"
+                  ".local")))
+
   ;; direct projectile to look for code in a specific folder.
   (setq projectile-project-search-path '("~/code")))
