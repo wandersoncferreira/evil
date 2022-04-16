dfcebf237107d0769249baf81d7525547a933837
Wanderson Ferreira <wand@hey.com>
2022-04-16 09:04:27 -0300

    change the cider modeline indicator

--- preferences/+clojure.el
+++ preferences/+clojure.el
@@ -1,11 +1,23 @@
 ;;; ../code/evil/preferences/+clojure.el -*- lexical-binding: t; -*-
 
+
+(defun bk/cider--modeline-info ()
+  "I'd like to see only cider[connected:port] or cider[not connected]"
+  (if-let* ((current-connection (ignore-errors (cider-current-repl))))
+      (with-current-buffer current-connection
+        (concat
+         (symbol-name cider-repl-type)
+         (when cider-mode-line-show-connection
+           (format ":connected:%s" (plist-get nrepl-endpoint :port)))))
+    "not connected"))
+
 (use-package! cider
   :init
   (setq cider-jdk-src-paths '("~/Downloads/clojure-1.10.3-sources" "~/Downloads/jvm11/source")
         cider-clojure-cli-command "~/code/dotfiles/clojure/clojure-bin-enriched"
         cider-save-file-on-load t
         cider-use-xref nil ;; use lsp
+        cider-mode-line '(:eval (format " cider[%s]" (bk/cider--modeline-info)))
         clojure-toplevel-inside-comment-form t)
   :config
   ;; remove the colors in the parens, I'm boring person
