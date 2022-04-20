ecfe01f28f1bd4eb9a426286c70c85d1d1f39e26
Wanderson Ferreira <wand@hey.com>
2022-04-20 15:24:08 -0300

    strict parens config

--- preferences/+parens.el
+++ preferences/+parens.el
@@ -1,12 +1,7 @@
 ;;; ../code/evil/preferences/+parens.el -*- lexical-binding: t; -*-
 
-(use-package! smartparens
-  :config
-  ;; fix bugs with smartparens and unbalanced pairs
-  (dolist (brace '("(" "{" "[" "'" "\""))
-    (sp-pair brace nil :unless nil))
-  :hook ((clojure-mode . smartparens-strict-mode)
-         (emacs-lisp-mode . smartparens-strict-mode)))
+(after! smartparens
+  (add-hook! (clojure-mode emacs-lisp-mode) #'smartparens-strict-mode))
 
 (map! :leader
       (:prefix ("l" . "lisp")
