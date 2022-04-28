2510283866bd41e24417cc9a0f7ae60abdebb3b9
Wanderson Ferreira <wand@hey.com>
2022-04-27 22:41:57 -0300

    remove comments

--- preferences/+rss.el
+++ preferences/+rss.el
@@ -7,8 +7,4 @@
           "https://xkcd.com/atom.xml"
           "https://emacsredux.com/atom.xml"
           "https://okmij.org/ftp/rss.xml"
-          "http://planet.clojure.in/atom.xml"))
-
-  ;; (set-face-attribute 'elfeed-search-title-face nil :foreground "dim gray")
-  ;; (set-face-attribute 'elfeed-search-unread-title-face nil :foreground "blue")
-  )
+          "http://planet.clojure.in/atom.xml")))
