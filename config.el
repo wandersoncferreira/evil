58d62ec3f442bd5a32d319ced3c7621a01f3e1c0
Wanderson Ferreira <wand@hey.com>
2022-05-01 15:47:24 -0300

    handle gpg files in the same loop

--- config.el
+++ config.el
@@ -1,5 +1,7 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+(require 's)
+
 (defvar list-of-preferences
   '("doom"
     "functions"
@@ -13,14 +15,11 @@
     "folds"
     "modeline"
     "completion"
+    "cisco.gpg"
     "rss"))
 
-(defvar list-of-gpg-preferences
-  '("cisco"))
-
 (dolist (cfg list-of-preferences)
-  (let ((path (format "preferences/+%s" cfg)))
-    (load! path)))
-
-(dolist (cfg list-of-gpg-preferences)
-  (bk/load-gpg-file cfg))
+  (if (s-ends-with? ".gpg" cfg)
+      (bk/load-gpg-file (car (s-split ".gpg" cfg)))
+    (let ((path (format "preferences/+%s" cfg)))
+      (load! path))))
