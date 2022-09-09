cb5dfb2c12498efa7c3b609ab0e4f0e5fd48428d
Wanderson Ferreira <wand@hey.com>
2022-09-08 23:23:53 -0300

    sync docs

--- modules/custom/skin/config.el
+++ modules/custom/skin/config.el
@@ -124,7 +124,7 @@
   (setq doom-theme 'vim-colors))
 
 ;; current theme
-(enable-modus-vivendi)
+(enable-modus-operandi)
 
 (map! :leader
       :desc "Delete other windows" "wo" #'delete-other-windows
