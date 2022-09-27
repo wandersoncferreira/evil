d859449a85a9cd7c581d2ebacb8fa9237da7cfd3
Wanderson Ferreira <wand@hey.com>
2022-09-27 09:11:16 -0300

    folder anatomy

--- config.el
+++ config.el
@@ -5,3 +5,10 @@
 ;; we know what we are doing, don't we?
 ;; all my personal configurations are written in the `modules/custom' folder
 ;; and loaded in `init.el' under the `:custom' keys
+
+;; project anatomy
+;;
+;; ./landlord - elisp packages that I own and are distributed as packages
+;; ./modules  - configuration defined in `:custom' keys in init.el file
+;; ./themes   - custom themes not available as elisp packages
+;; ./docs     - too many words to say: "learn with practice, use Emacs + Vim"
