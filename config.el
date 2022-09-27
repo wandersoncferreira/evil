9565119dd86d03346004666ebe480dca39f8dfcf
Wanderson Ferreira <wand@hey.com>
2022-09-27 09:08:10 -0300

    config docs

--- config.el
+++ config.el
@@ -1,6 +1,7 @@
 ;;; ../code/evil/config.el -*- lexical-binding: t; -*-
 
-;; empty file added on purpose so Doom will not create it automatically
-;; when I do clean installs
+;; empty file added on purpose.
+;; when you perform a clean install, Doom will automatically create this file.
+;; we know what we are doing, don't we?
 ;; all my personal configurations are written in the `modules/custom' folder
 ;; and loaded in `init.el' under the `:custom' keys
