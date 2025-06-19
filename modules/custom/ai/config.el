631e3212d189d108b99f617e16b79035a53fc23e
Wanderson Ferreira <wand@hey.com>
2025-06-19 15:36:55 -0300

    gptel

--- modules/custom/ai/config.el
+++ modules/custom/ai/config.el
@@ -1,11 +1,11 @@
 ;;; custom/ai/config.el -*- lexical-binding: t; -*-
 
-(use-package! gptel
-  :config
-  (setq gptel-default-mode 'org-mode
-        gptel-log-level 'info
-        gptel-use-tools t
-        gptel-backend (gptel-make-gh-copilot "Copilot")
-        gptel-model 'claude-sonnet-4)
-  (require 'gptel-integrations) ;; required for mcp
-  )
+(require 'gptel)
+
+(setq gptel-default-mode 'org-mode
+      gptel-log-level 'info
+      gptel-use-tools t
+      gptel-backend (gptel-make-gh-copilot "Copilot")
+      gptel-model 'claude-sonnet-4)
+
+(require 'gptel-integrations)
